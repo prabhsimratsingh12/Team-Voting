04283e1a060c738ad4ec831b237230e0588c5d98
prabhsimratsingh12 <74855327+prabhsimratsingh12@users.noreply.github.com>
2022-02-07 23:09:36 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDiYpMpJBoTtDbJ6B9dD47eOmuc8ie4rFI",
+  authDomain: "classtest-3b2e3.firebaseapp.com",
+  projectId: "classtest-3b2e3",
+  storageBucket: "classtest-3b2e3.appspot.com",
+  messagingSenderId: "195933585072",
+  appId: "1:195933585072:web:20f0316e6bc202782dc575"
+};
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
